d77866f14a694321bd9a4d466d423232b432c023
lilizeng99 <lilizeng99@gmail.com>
2016-06-07 11:00:55 -0700

    got data in backstage for popup window

--- development/js/table.js
+++ development/js/table.js
@@ -1,14 +1,34 @@
-var current_segment_ids = [1212,48, 23]; // a list of segment id, changing as the current window changes
 var cached_segment;
+var sorted_current_segment;
+var current_segment_detail;
 // should change to segments_in_view
 var sorted_segment_info = function (segments){
         var sorted_segments = [];
-        for (var segment in segments)
-            //console.log("From the function: ", segment)
-            sorted_segments.push([segments[segment]["segment_name"], segments[segment]["# of attempts"]]);
+        for (var segment in segments){
+            sorted_segments.push([segments[segment]["segment_name"], segments[segment]["# of attempts"], 
+                segments[segment]["# of athletes"],  segments[segment]["distance"], 
+                segments[segment]["average_grade"], segments[segment]["elevation_low"], 
+                segments[segment]["elevation_high"], segments[segment]["elevation_diff"], 
+                segments[segment]["climb_category"], 
+                segments[segment]["athlete_showcase"]]);
+        }
 
         sorted_segments.sort(function(a, b) {return a[1] - b[1]});
         return sorted_segments.reverse();
+        /*
+         for (var segment in segments){
+            sorted_segments.push({"segment_name" : segments[segment]["segment_name"], "# of attempts": segments[segment]["# of attempts"], 
+                "# of athletes" : segments[segment]["# of athletes"],  "elevation_low" : segments[segment]["elevation_low"], 
+                "elevation_high" : segments[segment]["elevation_high"], "elevation_diff" : segments[segment]["elevation_diff"], 
+                "distance" : segments[segment]["distance"], "climb_category" : segments[segment]["climb_category"], 
+                "athlete_showcase" : segments[segment]["athlete_showcase"]});
+        }
+        console.log("From the function: ", sorted_segments);
+            
+
+        sorted_segments.sort(function(a, b) {return a["# of attempts"] - b["# of attempts"]});
+        return sorted_segments.reverse();
+        */
     }
 
 var display_top_segments = function(sorted_segments){
@@ -28,12 +48,14 @@ var display_top_segments = function(sorted_segments){
 
         var span_name = document.createElement('span');
         span_name.className += 'span-name';
+        span_name.id += 'segment-' + i;
 
         var span_count = document.createElement('span');
         span_count.className += 'span_count';
-
+        span_count.id += 'segment-' + i;
         var li_segment = document.createElement('li');
-        li_segment.className += 'li-segment';
+        li_segment.className += 'li-segment-s';
+        li_segment.id += 'segment-' + i;
         var t_name = document.createTextNode(name);
         var t_count = document.createTextNode(attempt_count);
 
@@ -64,16 +86,54 @@ function render_segment_tb(segments_in_view){
         }
     });
 
-    //console.log(segment)
-  
-    var sorted_current_segment_info = sorted_segment_info(current_segment_info);
-    sorted_current_segment_info = sorted_current_segment_info.slice(0,30);
-    display_top_segments(sorted_current_segment_info);
+ 
+    sorted_current_segment = sorted_segment_info(current_segment_info);
+    sorted_current_segment = sorted_current_segment.slice(0,30);
+    display_top_segments(sorted_current_segment);
+    click_table_row();
 }
+
 
 
 //var segment = d3.json("data/processed_segment.json");
 //render_segment_tb(segments_in_view);
+
+function popUp(){
+    var svgContainer = d3.select("#right-column")
+    .append("svg")
+    .attr("width", 200)
+    .attr("height", 200);
+
+    var circle = svgContainer
+    .append("circle")
+    .color("black")
+    .attr("cx", 30)
+    .attr("cy", 30)
+    .attr("r", 20)
+}
+
+function click_table_row(){
+    var segment_entires = document.getElementsByClassName('li-segment-s');
+    for (var i = 0; i < segment_entires.length; i++) {
+        segment_entires[i].onclick = function(e) {
+
+            var clicked_row_index = e.target.id.split("-")[1];
+            
+            current_segment_detail = sorted_current_segment[clicked_row_index];
+            popUp();
+            /*
+            var clicked_segment_id = sorted_current_segment[clicked_row_index][2];
+            console.log(clicked_segment_id);
+            
+            cached_segment.forEach(function(d) {
+                if (d["segment_id"] == clicked_segment_id){
+                    console.log(d);
+                }
+            });
+            */
+        }
+    }
+}
 
 d3.json("data/processed_segment.json", function(error, data){
     var current_segment_info = [];
@@ -89,11 +149,11 @@ d3.json("data/processed_segment.json", function(error, data){
     });
 
 
-    var sorted_current_segment_info = sorted_segment_info(current_segment_info);
+    sorted_current_segment = sorted_segment_info(current_segment_info);
 
-    sorted_current_segment_info = sorted_current_segment_info.slice(0,30);
-    display_top_segments(sorted_current_segment_info);
-
+    sorted_current_segment = sorted_current_segment.slice(0,30);
+    display_top_segments(sorted_current_segment);
+    click_table_row();
 
 });
 
